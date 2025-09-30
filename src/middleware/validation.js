e863ed0da5ddacd4642f3222932b01a026adabce
JanviMangukiya <janvimangukiya1708@gmail.com>
2025-09-30 12:21:54 +0530

    Update Validation

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -34,11 +34,7 @@ const validationLogin = ((req, res, next) => {
     if(!password) {
         return res.json({message: "Passowrd are Required..." });
     }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 27d7991dd0ccecd5a13bb1e4da0d4a952f1a82b7
+    
     if(!emailValid.test(userName) && !numberValid.test(userName)) {
         return res.json({ message: "Invalid Email or Mobile Number" });
     }
